60c143f9c23c08a2aa9261ffeacc78b2f7571312
AmberLehmann <132320992+AmberLehmann@users.noreply.github.com>
2024-09-14 18:25:00 -0400

    "added options"

--- home.nix
+++ home.nix
@@ -109,44 +109,44 @@
       #     config = "colorscheme gruvbox";
       #   }
 
-      #   neodev-nvim
+        neodev-nvim
 
-      #   nvim-cmp 
-      #   {
-      #     plugin = nvim-cmp;
-      #     config = toLuaFile ./nvim/plugin/cmp.lua;
-      #   }
+        nvim-cmp 
+        {
+          plugin = nvim-cmp;
+          config = toLuaFile ./nvim/plugin/cmp.lua;
+        }
 
-      #   {
-      #     plugin = telescope-nvim;
-      #     config = toLuaFile ./nvim/plugin/telescope.lua;
-      #   }
+        {
+          plugin = telescope-nvim;
+          config = toLuaFile ./nvim/plugin/telescope.lua;
+        }
 
-      #   telescope-fzf-native-nvim
+        telescope-fzf-native-nvim
 
-      #   cmp_luasnip
-      #   cmp-nvim-lsp
+        cmp_luasnip
+        cmp-nvim-lsp
 
-      #   luasnip
-      #   friendly-snippets
+        luasnip
+        friendly-snippets
 
 
-      #   lualine-nvim
-      #   nvim-web-devicons
+        lualine-nvim
+        nvim-web-devicons
 
-      #   {
-      #     plugin = (nvim-treesitter.withPlugins (p: [
-      #       p.tree-sitter-nix
-      #       p.tree-sitter-vim
-      #       p.tree-sitter-bash
-      #       p.tree-sitter-lua
-      #       p.tree-sitter-python
-      #       p.tree-sitter-json
-      #     ]));
-      #     config = toLuaFile ./nvim/plugin/treesitter.lua;
-      #   }
+        {
+          plugin = (nvim-treesitter.withPlugins (p: [
+            p.tree-sitter-nix
+            p.tree-sitter-vim
+            p.tree-sitter-bash
+            p.tree-sitter-lua
+            p.tree-sitter-python
+            p.tree-sitter-json
+          ]));
+          config = toLuaFile ./nvim/plugin/treesitter.lua;
+        }
 
-      #   vim-nix
+        vim-nix
 
       #   # {
       #   #   plugin = vimPlugins.own-onedark-nvim;
