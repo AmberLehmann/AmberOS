c4265981b141e36d939e96352f76fa312e0f7be6
AmberLehmann <132320992+AmberLehmann@users.noreply.github.com>
2024-09-16 11:51:42 -0400

    "fixed shell.nix"

--- shell.nix
+++ shell.nix
@@ -2,14 +2,12 @@
 
 { pkgs ? import <nixpkgs> {} }:
 
-{
-  pkgs.mkShell = {
-    packages = with pkgs; [ 
-      
-      julia_19-bin # For cmsc460
-      vimPlugins.julia-vim # For cmsc460
-      qtspim # For cmsc411
-      
-    ];
-  };
+
+pkgs.mkShell {
+  packages = with pkgs; [ 
+    julia_19-bin # For cmsc460
+    vimPlugins.julia-vim # For cmsc460
+    qtspim # For cmsc411
+  ];
 }
+
