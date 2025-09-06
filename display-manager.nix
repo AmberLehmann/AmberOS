55218db56dc6e962f951b1111fa0b250b84e6f9c
AmberLehmann <132320992+AmberLehmann@users.noreply.github.com>
2025-09-06 00:31:39 -0400

    Change Display Manager Theme

--- display-manager.nix
+++ display-manager.nix
@@ -2,23 +2,15 @@
 
 {
   environment.systemPackages = with pkgs; [
-
     libsForQt5.qt5.qtsvg
     libsForQt5.qt5.qtgraphicaleffects
     libsForQt5.qt5.qtquickcontrols2
-    (catppuccin-sddm.override {
-      flavor = "mocha";
-      font  = "Noto Sans";
-      fontSize = "9";
-      background = "${./wallpapers/nix-black-4k.png}";
-      loginBackground = true;
-    }
-  )];
+  ];
   services.displayManager.sddm = {
     enable = true;
-    theme = "catppuccin-mocha";
+    theme = "where_is_my_sddm_theme";
     wayland.enable = true;
-    package = pkgs.kdePackages.sddm;
+    # package = pkgs.kdePackages.sddm;
   };
 }
 
